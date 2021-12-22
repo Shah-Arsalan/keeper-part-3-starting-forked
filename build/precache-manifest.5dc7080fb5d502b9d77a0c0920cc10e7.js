94957d63bc84331030818f54483d11258d01f358
Shah-Arsalan <shaharslan2278@gmail.com>
2021-12-22 19:11:14 +0530

    Created build

---
+++ build/precache-manifest.5dc7080fb5d502b9d77a0c0920cc10e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a6843c2a9c3b11313dd3818e26dcb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91dcddfb80aa5ef12447",
+    "url": "/static/js/2.32b9d141.chunk.js"
+  },
+  {
+    "revision": "7526355a1f305a08beac",
+    "url": "/static/js/main.32821c62.chunk.js"
+  },
+  {
+    "revision": "921d0da837860e5eca9c",
+    "url": "/static/js/runtime-main.0ce15bf1.js"
+  }
+]);
